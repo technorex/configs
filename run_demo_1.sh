618a315d224a326cb6a96ecfe6ec22cc8af21a0c
technorex <d.nogacz@gmail.com>
2018-12-20 13:01:31 +0100

    added run_demo_1

---
+++ run_demo_1.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+sleep_t=1
+set_led="./set_led"
+
+eval $set_led "0000"
+sleep $sleep_t
+eval $set_led "0110"
+sleep $sleep_t
+eval $set_led "1001"
+sleep $sleep_t
+eval $set_led "0220"
+sleep $sleep_t
+eval $set_led "2002"
+sleep $sleep_t
+eval $set_led "0330"
+sleep $sleep_t
+eval $set_led "3003"
+
+sleep $sleep_t
+eval $set_led "0000"
+sleep $sleep_t
+eval $set_led "0220"
+sleep $sleep_t
+eval $set_led "2002"
+sleep $sleep_t
+eval $set_led "2112"
+sleep $sleep_t
+eval $set_led "1212"
+sleep $sleep_t
+eval $set_led "2121"
+sleep $sleep_t
+eval $set_led "1212"
+sleep $sleep_t
+eval $set_led "2121"
+
+sleep $sleep_t
+eval $set_led "2123"
+sleep $sleep_t
+eval $set_led "1233"
+sleep $sleep_t
+eval $set_led "1333"
+sleep $sleep_t
+eval $set_led "3333"
+sleep $sleep_t
+eval $set_led "3331"
+sleep $sleep_t
+eval $set_led "3311"
+sleep $sleep_t
+eval $set_led "3111"
+sleep $sleep_t
+eval $set_led "1111"
+sleep $sleep_t
+eval $set_led "0000"
+sleep $sleep_t
+eval $set_led "1111"
+sleep $sleep_t
+eval $set_led "0000"
