fb6c2b7ce749f2e1701d22b686873e0bd38db620
technorex <d.nogacz@gmail.com>
2019-01-15 13:08:21 +0100

    update demo

--- run_demo_2.sh
+++ run_demo_2.sh
@@ -20,21 +20,4 @@ eval $set_led "0011"
 sleep $sleep_t
 eval $set_led "0001"
 sleep $sleep_t
-eval $set_led "2000"
-sleep $sleep_t
-
-
-eval $set_led "2200"
-sleep $sleep_t
-eval $set_led "2220"
-sleep $sleep_t
-eval $set_led "2222"
-sleep $sleep_t
-eval $set_led "0222"
-sleep $sleep_t
-
-eval $set_led "0022"
-sleep $sleep_t
-eval $set_led "0002"
-sleep $sleep_t
 eval $set_led "0000"
